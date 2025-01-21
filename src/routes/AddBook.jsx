3656c512010b1f7d31428f6645a29d027ddc774f
Ruth <ruthasafuadjaye@yahoo.com>
2025-01-21 23:59:17 +0200

    merge 107 into main

--- src/routes/AddBook.jsx
+++ src/routes/AddBook.jsx
@@ -14,16 +14,13 @@ import { bookGenres } from '../genres';
 import { Stack, Typography } from '@mui/material';
 import defaultBook from '../assets/defaultBook.svg';
 
-
-//function to add new book to the current books available already
 function AddBook() {
   const { alert, post } = useAxios('http://localhost:3000');
   const [rateValue, setRateValue] = useState(3);
-  //const [hover, setHover] = useState (0);
   const [book, setBook] = useState({
     author: '',
     name: '',
-    genres: [], // a collection of boook types kept in an array
+    genres: [],
     completed: false,
     start: null,
     end: null,
@@ -31,7 +28,6 @@ function AddBook() {
     img: defaultBook, // missing parameter on the form. Also in postHandler function
   });
 
-    // choosing the genre of book. add a new book with genre(s). if book falls under more than one genre, split the types with ','.
   const genreChangeHandler = (event) => {
     const { value } = event.target;
     setBook({
@@ -40,7 +36,6 @@ function AddBook() {
     });
   };
 
-  // rating of the book with the rating( number of stars) of choice.
   const rateChangeHandler = (event) => {
     const { value } = event.target;
     setBook({
@@ -49,7 +44,6 @@ function AddBook() {
     });
   };
 
-  // a function to handle when form is submitted
   const addBookHandler = (e) => {
     const { name, value, checked, type } = e.target;
     if (type === 'checkbox' && name === 'completed') {
@@ -59,8 +53,29 @@ function AddBook() {
     }
   };
 
+   /*
   function postHandler() {
     post('books', book);
+  }
+  */
+
+  async function postHandler() {
+    try{
+      const response = await post ('books', book);
+      if (response.status === 201) {
+        setBook ({
+          author: '',
+          name: '',
+          genres: [],
+          completed: false,
+          start: null,
+          end: null,
+          stars: null,
+          img: '' 
+        })
+      }
+    }
+    catch (error) {console.log(error)}
   }
 
   return (
@@ -70,7 +85,7 @@ function AddBook() {
         alignItems="stretch"
         sx={{ my: 2, mx: 'auto', width: '25%' }}
       >
-        {alert.show && <Alert severity={alert.type} autoHideDuration={5000}>{alert.message}</Alert>}
+        {alert.show && <Alert severity={alert.type}>{alert.message}</Alert>}
         <Typography variant="h4" component="h2" sx={{ my: 10 }}>
           Add a book
         </Typography>
@@ -120,20 +135,12 @@ function AddBook() {
           <Rating
             name="stars"
             value={rateValue}
-            //onClick={rateChangeHandler}
+            onClick={rateChangeHandler}
             size="large"
             onChange={(event, newValue) => {
               setRateValue(newValue);
-              //rateChangeHandler(event, newValue);
             }}
-            onChangeActive={(event, setHover)=> {
-              if (setHover !== -1) {
-                setRateValue(setHover); 
-              }}}
           />
-            <Typography variant="subtitle1">
-            {rateValue !== null ? `Rating: ${rateValue}` : ' '}
-          </Typography>
         </Stack>
         <Button variant="contained" type="submit">
           Add new
